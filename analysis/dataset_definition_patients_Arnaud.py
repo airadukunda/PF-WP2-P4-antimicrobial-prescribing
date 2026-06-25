7ce2a2781df7b91822b752764cfb39bf1f0695b5
lshtmpharmacyfirst <Arnaud.Iradukunda@lshtm.ac.uk>
2026-06-25 15:27:20 +0000

    bb

--- analysis/dataset_definition_patients_Arnaud.py
+++ analysis/dataset_definition_patients_Arnaud.py
@@ -1520,16 +1520,18 @@ for name, codes in all_conditions_gp_codes.items():
 
     # A&E attendances with the condition
     ae_primary = ae_events.where(ae_events.diagnosis_01.is_in(codes))
-    attendance_ids = ae_primary.attendance_id               # attendance_ids = ae_primary.id
+    #//attendance_ids = ae_primary.attendance_id               # attendance_ids = ae_primary.id
+    attendance_ids = ae_primary.id
     # All records from those attendances
-    ae_condition_events = select_events_by_attendance_id(ae_events,attendance_ids,)
+    #//ae_condition_events = select_events_by_attendance_id(ae_events,attendance_ids,)
+    ae_condition_events = ae_events.where(ae_events.id.is_in(attendance_ids))
     # Any condition-specific medication
-    count_medication, count_medication_episode = has_event_count( ae_condition_events,codelists.condition_specific_medications_dict[name],)
+    count_medication, count_medication_episode = has_event_count( ae_condition_events,codelists.pharmacy_first_condition_specific_medications_dict[name],)
 
     setattr(dataset,f"numerator_ae_medication_{name}",count_medication,)
     setattr(dataset,f"numerator_ae_medication_episode_{name}", count_medication_episode,)
     # First- and second-line medications
-    for medication_name, medication_codes in (codelists.first_secondline_medications[name].items()):
+    for medication_name, medication_codes in (codelists.pf_first_secondline_medications[name].items()):
         count_medication, count_medication_episode = has_event_count(ae_condition_events,medication_codes,)
         setattr(dataset,f"numerator_ae_{medication_name}_{name}",count_medication,)
         setattr( dataset,f"numerator_ae_{medication_name}_episode_{name}",count_medication_episode,)
