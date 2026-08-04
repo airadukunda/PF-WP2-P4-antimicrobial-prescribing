dd552a0767c3e61f51d6b0b6fe19a31fb837f414
airadukunda <107347344+airadukunda@users.noreply.github.com>
2026-08-04 15:21:21 +0000

    measures

--- analysis/practice_variables/process_measures_to_practice_dataset_Arnaud.py
+++ analysis/practice_variables/process_measures_to_practice_dataset_Arnaud.py
@@ -48,21 +48,184 @@ pf_nitrofurantoin = (
     [["practice", "stp", "region", "interval_start", "pf_nitrofurantoin"]]
 )
 
+# NEW: PF medication measures
+
+pf_medication_uti = (
+    df[df["measure"] == "pf_medication_uti"]
+    .rename(columns={"numerator": "pf_medication_uti"})
+    [["practice", "stp", "region", "interval_start", "pf_medication_uti"]]
+)
+
+pf_medication_sinusitis = (
+    df[df["measure"] == "pf_medication_sinusitis"]
+    .rename(columns={"numerator": "pf_medication_sinusitis"})
+    [["practice", "stp", "region", "interval_start", "pf_medication_sinusitis"]]
+)
+
+pf_medication_insectbite = (
+    df[df["measure"] == "pf_medication_insectbite"]
+    .rename(columns={"numerator": "pf_medication_insectbite"})
+    [["practice", "stp", "region", "interval_start", "pf_medication_insectbite"]]
+)
+
+pf_medication_otitismedia = (
+    df[df["measure"] == "pf_medication_otitismedia"]
+    .rename(columns={"numerator": "pf_medication_otitismedia"})
+    [["practice", "stp", "region", "interval_start", "pf_medication_otitismedia"]]
+)
+
+pf_medication_sorethroat = (
+    df[df["measure"] == "pf_medication_sorethroat"]
+    .rename(columns={"numerator": "pf_medication_sorethroat"})
+    [["practice", "stp", "region", "interval_start", "pf_medication_sorethroat"]]
+)
+
+pf_medication_shingles = (
+    df[df["measure"] == "pf_medication_shingles"]
+    .rename(columns={"numerator": "pf_medication_shingles"})
+    [["practice", "stp", "region", "interval_start", "pf_medication_shingles"]]
+)
+
+pf_medication_impetigo = (
+    df[df["measure"] == "pf_medication_impetigo"]
+    .rename(columns={"numerator": "pf_medication_impetigo"})
+    [["practice", "stp", "region", "interval_start", "pf_medication_impetigo"]]
+)
+
+pf_medication_all_conditions = (
+    df[df["measure"] == "pf_medication_all_conditions"]
+    .rename(columns={"numerator": "pf_medication_all_conditions"})
+    [["practice", "stp", "region", "interval_start", "pf_medication_all_conditions"]]
+)
+
+#General practice level
+# NEW: GP medication measures
+
+gp_medication_uti = (
+    df[df["measure"] == "gp_medication_uti"]
+    .rename(columns={"numerator": "gp_medication_uti"})
+    [["practice", "stp", "region", "interval_start", "gp_medication_uti"]]
+)
+
+gp_medication_sinusitis = (
+    df[df["measure"] == "gp_medication_sinusitis"]
+    .rename(columns={"numerator": "gp_medication_sinusitis"})
+    [["practice", "stp", "region", "interval_start", "gp_medication_sinusitis"]]
+)
+
+gp_medication_insectbite = (
+    df[df["measure"] == "gp_medication_insectbite"]
+    .rename(columns={"numerator": "gp_medication_insectbite"})
+    [["practice", "stp", "region", "interval_start", "gp_medication_insectbite"]]
+)
+
+gp_medication_otitismedia = (
+    df[df["measure"] == "gp_medication_otitismedia"]
+    .rename(columns={"numerator": "gp_medication_otitismedia"})
+    [["practice", "stp", "region", "interval_start", "gp_medication_otitismedia"]]
+)
+
+gp_medication_sorethroat = (
+    df[df["measure"] == "gp_medication_sorethroat"]
+    .rename(columns={"numerator": "gp_medication_sorethroat"})
+    [["practice", "stp", "region", "interval_start", "gp_medication_sorethroat"]]
+)
+
+gp_medication_shingles = (
+    df[df["measure"] == "gp_medication_shingles"]
+    .rename(columns={"numerator": "gp_medication_shingles"})
+    [["practice", "stp", "region", "interval_start", "gp_medication_shingles"]]
+)
+
+gp_medication_impetigo = (
+    df[df["measure"] == "gp_medication_impetigo"]
+    .rename(columns={"numerator": "gp_medication_impetigo"})
+    [["practice", "stp", "region", "interval_start", "gp_medication_impetigo"]]
+)
+
+gp_medication_all_conditions = (
+    df[df["measure"] == "gp_medication_all_conditions"]
+    .rename(columns={"numerator": "gp_medication_all_conditions"})
+    [["practice", "stp", "region", "interval_start", "gp_medication_all_conditions"]]
+)
+
+# NEW: GP control medication measures
+
+gp_medication_acutebronchitis_control = (
+    df[df["measure"] == "gp_medication_acutebronchitis_control"]
+    .rename(columns={"numerator": "gp_medication_acutebronchitis_control"})
+    [["practice", "stp", "region", "interval_start", "gp_medication_acutebronchitis_control"]]
+)
+
+gp_medication_conjunctivitisallergic_control = (
+    df[df["measure"] == "gp_medication_conjunctivitisallergic_control"]
+    .rename(columns={"numerator": "gp_medication_conjunctivitisallergic_control"})
+    [["practice", "stp", "region", "interval_start", "gp_medication_conjunctivitisallergic_control"]]
+)
+
+gp_medication_vulvovaginalcandidiasis_control = (
+    df[df["measure"] == "gp_medication_vulvovaginalcandidiasis_control"]
+    .rename(columns={"numerator": "gp_medication_vulvovaginalcandidiasis_control"})
+    [["practice", "stp", "region", "interval_start", "gp_medication_vulvovaginalcandidiasis_control"]]
+)
 
 df_wide = pop.merge(appt_scheduled,on=["practice", "stp", "region", "interval_start"],how="left")
 df_wide = df_wide.merge(appt_seen,on=["practice", "stp", "region", "interval_start"],how="left")
 df_wide = df_wide.merge(pf, on=["practice", "stp", "region", "interval_start"], how="left")
 df_wide = df_wide.merge(pf_uti_consultation, on=["practice", "stp", "region", "interval_start"], how="left")
 df_wide = df_wide.merge(pf_uti_eligible, on=["practice", "stp", "region", "interval_start"], how="left")
-# NEW: Merge medication measure
+# NEW: PF medication measures
 df_wide = df_wide.merge(pf_nitrofurantoin,on=["practice", "stp", "region", "interval_start"],how="left")
+df_wide = df_wide.merge(pf_medication_uti,on=["practice", "stp", "region", "interval_start"],how="left",)
+df_wide = df_wide.merge(pf_medication_sinusitis,on=["practice", "stp", "region", "interval_start"],how="left",)
+df_wide = df_wide.merge(pf_medication_insectbite,on=["practice", "stp", "region", "interval_start"],how="left",)
+df_wide = df_wide.merge(pf_medication_otitismedia,on=["practice", "stp", "region", "interval_start"],how="left",)
+df_wide = df_wide.merge(pf_medication_sorethroat,on=["practice", "stp", "region", "interval_start"],how="left",)
+df_wide = df_wide.merge(pf_medication_shingles,on=["practice", "stp", "region", "interval_start"],how="left",)
+df_wide = df_wide.merge(pf_medication_impetigo,on=["practice", "stp", "region", "interval_start"],how="left",)
+df_wide = df_wide.merge(pf_medication_all_conditions,on=["practice", "stp", "region", "interval_start"],how="left",)
+# NEW: GP medication measures
+df_wide = df_wide.merge(gp_medication_uti,on=["practice", "stp", "region", "interval_start"],how="left",)
+df_wide = df_wide.merge(gp_medication_sinusitis,on=["practice", "stp", "region", "interval_start"],how="left",)
+df_wide = df_wide.merge(gp_medication_insectbite,on=["practice", "stp", "region", "interval_start"],how="left",)
+df_wide = df_wide.merge(gp_medication_otitismedia,on=["practice", "stp", "region", "interval_start"],how="left",)
+df_wide = df_wide.merge(gp_medication_sorethroat,on=["practice", "stp", "region", "interval_start"],how="left",)
+df_wide = df_wide.merge(gp_medication_shingles,on=["practice", "stp", "region", "interval_start"],how="left",)
+df_wide = df_wide.merge(gp_medication_impetigo,on=["practice", "stp", "region", "interval_start"],how="left",)
+df_wide = df_wide.merge(gp_medication_all_conditions,on=["practice", "stp", "region", "interval_start"],how="left",)
+# NEW: GP control medication measures
+df_wide = df_wide.merge(gp_medication_acutebronchitis_control,on=["practice", "stp", "region", "interval_start"],how="left",)
+df_wide = df_wide.merge(gp_medication_conjunctivitisallergic_control,on=["practice", "stp", "region", "interval_start"],how="left",)
+df_wide = df_wide.merge(gp_medication_vulvovaginalcandidiasis_control,on=["practice", "stp", "region", "interval_start"],how="left",)
 for col in [
     "appointments_scheduled",
     "appointments_seen",
     "pf_consultation_general",
     "pf_consultation_uti",
     "populationeligible_uuti",
-    "pf_nitrofurantoin",     # NEW: Fill missing medication values
+    # NEW: medication values(protocol 4)
+    "pf_nitrofurantoin",  
+    "pf_medication_uti",
+    "pf_medication_sinusitis",
+    "pf_medication_insectbite",
+    "pf_medication_otitismedia",
+    "pf_medication_sorethroat",
+    "pf_medication_shingles",
+    "pf_medication_impetigo",
+    "pf_medication_all_conditions",
+    # NEW: GP medication measures
+    "gp_medication_uti",
+    "gp_medication_sinusitis",
+    "gp_medication_insectbite",
+    "gp_medication_otitismedia",
+    "gp_medication_sorethroat",
+    "gp_medication_shingles",
+    "gp_medication_impetigo",
+    "gp_medication_all_conditions",
+    # NEW: GP control medication measures
+    "gp_medication_acutebronchitis_control",
+    "gp_medication_conjunctivitisallergic_control",
+    "gp_medication_vulvovaginalcandidiasis_control",
 ]:
     df_wide[col] = df_wide[col].fillna(0)
 
