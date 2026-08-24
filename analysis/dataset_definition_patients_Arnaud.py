41c16c31a67976d5f3eb060c9af0ff82c6f5d953
airadukunda <107347344+airadukunda@users.noreply.github.com>
2026-08-24 00:02:54 +0000

    n

--- analysis/dataset_definition_patients_Arnaud.py
+++ analysis/dataset_definition_patients_Arnaud.py
@@ -83,7 +83,7 @@ from analysis.codelists import (
     ethnicity_group16_codelist,
     )
 dataset = create_dataset()
-dataset.configure_dummy_data(population_size=1000) # The size can be increased from 500 to 1000 pop.airadukunda
+dataset.configure_dummy_data(population_size=100) # The size can be increased from 500 to 1000 pop.airadukunda
 
 # One month time period (to start with this is Nov 25) 
 # start_date = "2025-10-31"     
